b33bed999acd82154b520a252c4b5d2691f60ba8
singhalisha829 <singhalisha829@gmail.com>
2026-07-28 18:56:38 +0530

    feat: accessibility added

--- src/nodes/BaseNode.js
+++ src/nodes/BaseNode.js
@@ -18,35 +18,49 @@ const BaseNode = ({ id, title, fields = [], handles = [], children }) => {
 
   return (
     <div style={{ width: 200, height: 80, border: "1px solid black" }}>
+      {/* title */}
       <div>{title}</div>
+
+      {/* custom content */}
+      {children && <div>{children}</div>}
+
+      {/* Fields */}
       {fields && (
         <div>
-          {fields.map((field) => (
-            <div key={field.name}>
-              <label>{field.label}:</label>
-              {field.type === "select" ? (
-                <select
-                  value={values[field.name]}
-                  onChange={(e) => handleChange(field, e.target.value)}
-                >
-                  {field.options.map((option) => (
-                    <option key={option.key} value={option.key}>
-                      {option.name}
-                    </option>
-                  ))}
-                </select>
-              ) : (
-                <input
-                  type={field.type}
-                  value={values[field.name]}
-                  onChange={(e) => handleChange(field, e.target.value)}
-                />
-              )}{" "}
-            </div>
-          ))}
+          {fields.map((field) => {
+            const fieldId = `${id}-${field.name}`;
+            return (
+              <div key={field.name}>
+                <label htmlFor={fieldId}>{field.label}:</label>
+                {field.type === "select" ? (
+                  <select
+                    id={fieldId}
+                    value={values[field.name]}
+                    aria-label={field.label}
+                    onChange={(e) => handleChange(field, e.target.value)}
+                  >
+                    {field.options.map((option) => (
+                      <option key={option.key} value={option.key}>
+                        {option.name}
+                      </option>
+                    ))}
+                  </select>
+                ) : (
+                  <input
+                    id={fieldId}
+                    type={field.type}
+                    aria-label={field.label}
+                    value={values[field.name]}
+                    onChange={(e) => handleChange(field, e.target.value)}
+                  />
+                )}{" "}
+              </div>
+            );
+          })}
         </div>
       )}
 
+      {/* Handles */}
       {handles &&
         handles.map((handle) => (
           <Handle
@@ -55,6 +69,8 @@ const BaseNode = ({ id, title, fields = [], handles = [], children }) => {
             position={handle.position}
             id={handle.id}
             style={{ ...handle.style }}
+            aria-label={`${handle.type} handle for ${title} node`}
+            tabIndex={0}
           />
         ))}
     </div>
